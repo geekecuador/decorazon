b32a1e68d4212be4e0a691c81d2bbd21738dce02
dpulloquinga <dapl@me.com>
2016-10-28 14:43:15 -0500

    ok
    ok

--- mashca/admin.py
+++ mashca/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Codigo, Proyecto,  Donaciones
+from .models import Codigo, Proyecto,  Donaciones,Donacion_Paypal
 # Register your models here.
 admin.site.register(Codigo)
 admin.site.register(Proyecto)
 admin.site.register(Donaciones)
+admin.site.register(Donacion_Paypal)
